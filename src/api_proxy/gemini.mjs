5907242cdb41cc6c0adb5b5d72cd7671e0560538
Ysansan <ysansan98@outlook.com>
2025-01-30 23:55:39 +0800

    fix bug

--- src/api_proxy/gemini.mjs
+++ src/api_proxy/gemini.mjs
@@ -22,11 +22,7 @@ async function handleGeminiAPIRequest(req) {
 		const response = await fetch(proxyReq);
 
 		// 非流式响应直接返回
-		return new Response(response.body, {
-			status: response.status,
-			statusText: response.statusText,
-			headers: response.headers,
-		});
+		return new Response(response.body, fixCors(response));
 	} catch (error) {
 		console.error('Gemini API request error:', error);
 		const errorMessage = error instanceof Error ? error.message : '未知错误';
@@ -39,67 +35,8 @@ async function handleGeminiAPIRequest(req) {
 	}
 }
 
-async function parseStream(chunk, controller) {
-	chunk = await chunk;
-	if (!chunk) { return; }
-	this.buffer += chunk;
-	do {
-		const match = this.buffer.match(responseLineRE);
-		if (!match) { break; }
-		controller.enqueue(match[1]);
-		this.buffer = this.buffer.substring(match[0].length);
-	} while (true); // eslint-disable-line no-constant-condition
-}
-
-async function parseStreamFlush(controller) {
-	if (this.buffer) {
-		console.error("Invalid data:", this.buffer);
-		controller.enqueue(this.buffer);
-	}
-}
-
-
-export default {
-	handleGeminiAPIRequest
-}
-
-
-async function toOpenAiStream (chunk, controller) {
-  const transform = transformResponseStream.bind(this);
-  const line = await chunk;
-  if (!line) { return; }
-  let data;
-  try {
-    data = JSON.parse(line);
-  } catch (err) {
-    console.error(line);
-    console.error(err);
-    const length = this.last.length || 1; // at least 1 error msg
-    const candidates = Array.from({ length }, (_, index) => ({
-      finishReason: "error",
-      content: { parts: [{ text: err }] },
-      index,
-    }));
-    data = { candidates };
-  }
-  const cand = data.candidates[0];
-  console.assert(data.candidates.length === 1, "Unexpected candidates count: %d", data.candidates.length);
-  cand.index = cand.index || 0; // absent in new -002 models response
-  if (!this.last[cand.index]) {
-    controller.enqueue(transform(data, false, "first"));
-  }
-  this.last[cand.index] = data;
-  if (cand.content) { // prevent empty data (e.g. when MAX_TOKENS)
-    controller.enqueue(transform(data));
-  }
-}
-
-async function toOpenAiStreamFlush (controller) {
-  const transform = transformResponseStream.bind(this);
-  if (this.last.length > 0) {
-    for (const data of this.last) {
-      controller.enqueue(transform(data, "stop"));
-    }
-    controller.enqueue("data: [DONE]" + delimiter);
-  }
-}
+const fixCors = ({ headers, status, statusText }) => {
+  headers = new Headers(headers);
+  headers.set("Access-Control-Allow-Origin", "*");
+  return { headers, status, statusText };
+};
